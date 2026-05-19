2a9dd50b521f427a77f63b094973041a86f54e1b
AlbertoFurlan20 <betobot10@gmail.com>
2026-05-19 15:09:33 +0200

    Refactored running scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,4 @@
-#/bin/bash
-
-# auto-gpt
-# pip install -r Auto-GPT/requirements.txt
-
-# crfm api
-# pip install crfm-helm
+#!/usr/bin/env bash
 
 # ML dependencies
 conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
