726372d6933f74f642a72468917ad68308167644
AlbertoFurlan20 <betobot10@gmail.com>
2026-05-20 14:36:34 +0200

    Added utility for toggling rebuild

--- scripts/rebuild_container.sh
+++ scripts/rebuild_container.sh
@@ -4,6 +4,8 @@ docker build -t mlagentbench-thesis .
 
 docker rm -f mlagentbench-thesis-ctr || true
 
+clear
+
 docker run --gpus all --user root -w /MLAgentBench \
   --name mlagentbench-thesis-ctr \
   -p 8001:8000 -p 8002:8002 \
